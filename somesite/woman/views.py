4e19214fd25402b7e5f8fb4180a514b65d1b25ab
Акакиев Акакий <BlackWiind@gmail.com>
2022-08-28 21:28:59 +1000

    Форма для добавления статьи

--- somesite/woman/views.py
+++ somesite/woman/views.py
@@ -1,6 +1,7 @@
 from django.http import HttpResponse, HttpResponseNotFound, Http404
 from django.shortcuts import render, redirect, get_object_or_404
 
+from .forms import AddPostForm
 from .models import *
 
 menu = [{'title': 'О сайте', 'url_name': 'about'},
@@ -28,7 +29,17 @@ def about(request):
 
 
 def addpage(request):
-    return HttpResponse("Добавление статьи")
+    if request.method == 'POST':
+        form = AddPostForm(request.POST, request.FILES)
+        if form.is_valid():
+            try:
+                Woman.objects.create(**form.cleaned_data)
+                return redirect('home')
+            except:
+                form.add_error(None, 'Ошибка при добавлении поста')
+    else:
+        form = AddPostForm()
+    return render(request, 'woman/addpage.html', {'form': form, 'menu': menu, 'title': 'Добавление статьи'})
 
 
 def contact(request):
