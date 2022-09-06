936f85d11bba94359c9f6d2fcf669fd66953ac39
Акакиев Акакий <BlackWiind@gmail.com>
2022-09-06 21:27:34 +1000

    Регистрация, авторизация

--- somesite/woman/utils.py
+++ somesite/woman/utils.py
@@ -5,7 +5,6 @@ from .models import Category
 menu = [{'title': 'О сайте', 'url_name': 'about'},
         {'title': 'Добавить статью', 'url_name': 'add_page'},
         {'title': 'Обратна связь', 'url_name': 'contact'},
-        {'title': 'Войти', 'url_name': 'login'},
         ]
 
 
--- somesite/woman/views.py
+++ somesite/woman/views.py
@@ -1,9 +1,13 @@
+from django.contrib.auth import logout, login
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
 from django.contrib.auth.mixins import LoginRequiredMixin
+from django.contrib.auth.views import LoginView
 from django.http import HttpResponse, HttpResponseNotFound, Http404
 from django.shortcuts import render, redirect, get_object_or_404
+from django.urls import reverse_lazy
 from django.views.generic import ListView, DetailView, CreateView
 
-from .forms import AddPostForm
+from .forms import AddPostForm, RegisterUserForm, LoginUserForm
 from .models import *
 from .utils import *
 
@@ -66,8 +70,8 @@ def contact(request):
     return HttpResponse("Обратная связь")
 
 
-def login(request):
-    return HttpResponse("Авторизация")
+# def login(request):
+#     return HttpResponse("Авторизация")
 
 
 def pageNotFound(request, exception):
@@ -113,17 +117,36 @@ class WomanCategory(DataMixin, ListView):
     def get_queryset(self):
         return Woman.objects.filter(cat__slug=self.kwargs['cat_slug'], is_published=True)
 
-# def show_category(request, cat_slug):
-#     posts = Woman.objects.filter(cat__slug=cat_slug)
-#
-#     if len(posts) == 0:
-#         raise Http404()
-#
-#     context = {
-#         'posts': posts,
-#         'menu': menu,
-#         'title': 'Отображение по категориям',
-#         'cat_selected': cat_slug,
-#
-#     }
-#     return render(request, 'woman/index.html', context=context)
+
+class RegisterUser(DataMixin, CreateView):
+    form_class = RegisterUserForm
+    template_name = 'woman/register.html'
+    success_url = reverse_lazy('login')
+
+    def get_context_data(self, *, object_list=None, **kwargs):
+        context = super().get_context_data(**kwargs)
+        c_def = self.get_user_context(title='Регистрация')
+        return dict(list(context.items()) + list(c_def.items()))
+
+    def form_valid(self, form):
+        user = form.save
+        login(self.request, user)
+        return redirect('home')
+
+
+class LoginUser(DataMixin, LoginView):
+    form_class = LoginUserForm
+    template_name = 'woman/login.html'
+
+    def get_context_data(self, *, object_list=None, **kwargs):
+        context = super().get_context_data(**kwargs)
+        c_def = self.get_user_context(title='Авторизация')
+        return dict(list(context.items()) + list(c_def.items()))
+
+    def get_success_url(self):
+        return reverse_lazy('home')
+
+
+def logout_user(request):
+    logout(request)
+    return redirect('home')
